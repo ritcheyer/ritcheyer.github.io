351fb47dbcc9e2dd06834a6b9f53ad4c66f9401b
Eric Ritchey <eric@eandjdesign.com>
2013-10-20 22:50:22 -0700

    moving spinner to top of js file

--- assets/js/paypal.js
+++ assets/js/paypal.js
@@ -72,6 +72,30 @@ $(document).ready(function(){
     return pattern.test(emailAddress);
   }
 
+  // spinner graphic
+  var opts, target, spinner;
+  opts = {
+    lines: 15, // The number of lines to draw
+    length: 0, // The length of each line
+    width: 11, // The line thickness
+    radius: 50, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 35, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#fff', // #rgb or #rrggbb or array of colors
+    speed: 1.3, // Rounds per second
+    trail: 64, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: true, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  };
+  target = document.getElementById('spinner');
+  spinner = new Spinner(opts).spin(target);
+
+
   $('textarea').autosize();
   
   // payment recipient selection
@@ -311,26 +335,4 @@ $(document).ready(function(){
     
   }
 
-
-  var opts = {
-    lines: 15, // The number of lines to draw
-    length: 0, // The length of each line
-    width: 11, // The line thickness
-    radius: 50, // The radius of the inner circle
-    corners: 1, // Corner roundness (0..1)
-    rotate: 35, // The rotation offset
-    direction: 1, // 1: clockwise, -1: counterclockwise
-    color: '#fff', // #rgb or #rrggbb or array of colors
-    speed: 1.3, // Rounds per second
-    trail: 64, // Afterglow percentage
-    shadow: false, // Whether to render a shadow
-    hwaccel: true, // Whether to use hardware acceleration
-    className: 'spinner', // The CSS class to assign to the spinner
-    zIndex: 2e9, // The z-index (defaults to 2000000000)
-    top: 'auto', // Top position relative to parent in px
-    left: 'auto' // Left position relative to parent in px
-  };
-  var target = document.getElementById('spinner');
-  var spinner = new Spinner(opts).spin(target);
-  
 });
